81dc954f1b8458553d917a826f416da452e45b4f
bleads2 <bleads2@gmail.com>
2023-05-02 00:34:17 -0700

    create two components home component and about component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'aboutus',component:AboutusComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
