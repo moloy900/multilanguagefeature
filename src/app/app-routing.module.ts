462816040af2d904113c6c127c80f1d285af3bd3
bleads2 <bleads2@gmail.com>
2023-05-02 23:40:20 -0700

    services module added in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'aboutus',component:AboutusComponent},
-  {path:'products', loadChildren:()=>import('./products/products.module').then(res=>res.ProductsModule)}
+  {path:'gallery',component:GalleryComponent},
+  {path:'products', loadChildren:()=>import('./products/products.module').then(res=>res.ProductsModule)},
+  {path:'service',loadChildren:()=>import('./services/services.module').then(res=>res.ServicesModule)}
 ];
 
 @NgModule({
