24b16e22f18cfd6c081e1f6dcf3f33d8d51401fb
bleads2 <bleads2@gmail.com>
2023-05-02 23:07:06 -0700

    gallery under all component move to products feature module

--- src/app/gallery/gallery-routing.module.ts
+++ src/app/gallery/gallery-routing.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TVComponent } from './tv/tv.component';
-import { LaptopComponent } from './laptop/laptop.component';
-import { MobileComponent } from './mobile/mobile.component';
-import { WashingMachineComponent } from './washing-machine/washing-machine.component';
+
 import { GalleryComponent } from './gallery.component';
 
 const routes: Routes = [
   {path:'', component:GalleryComponent, children:[
-    {path:'tv',component:TVComponent},
-  {path:'laptop',component:LaptopComponent},
-  {path:'mobile',component:MobileComponent},
-  {path:'washingmachine',component:WashingMachineComponent}
+   
   ]}
 ];
 
