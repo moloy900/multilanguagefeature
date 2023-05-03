5ba563879a79e8764690dfc4ec6700d9a5eab91f
bleads2 <bleads2@gmail.com>
2023-05-02 23:50:19 -0700

    contact component import in router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,28 @@ import { HomeComponent } from './home/home.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { ManageproductsComponent } from './manageproducts/manageproducts.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  {path:'',component:HomeComponent},
-  {path:'aboutus',component:AboutusComponent},
-  {path:'gallery',component:GalleryComponent},
-  {path:'products', loadChildren:()=>import('./products/products.module').then(res=>res.ProductsModule)},
-  {path:'service',loadChildren:()=>import('./services/services.module').then(res=>res.ServicesModule)},
-  {path:'manageproduct',component:ManageproductsComponent}
+  { path: '', component: HomeComponent },
+  { path: 'aboutus', component: AboutusComponent },
+  { path: 'gallery', component: GalleryComponent },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./products/products.module').then((res) => res.ProductsModule),
+  },
+  {
+    path: 'service',
+    loadChildren: () =>
+      import('./services/services.module').then((res) => res.ServicesModule),
+  },
+  { path: 'manageproduct', component: ManageproductsComponent },
+  { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
