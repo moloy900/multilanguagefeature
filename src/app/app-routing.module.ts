5cddcc057716f126c0f92654d6639da2e49e1450
bleads2 <bleads2@gmail.com>
2023-05-02 23:45:08 -0700

    import manageproducts component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { ManageproductsComponent } from './manageproducts/manageproducts.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'aboutus',component:AboutusComponent},
   {path:'gallery',component:GalleryComponent},
   {path:'products', loadChildren:()=>import('./products/products.module').then(res=>res.ProductsModule)},
-  {path:'service',loadChildren:()=>import('./services/services.module').then(res=>res.ServicesModule)}
+  {path:'service',loadChildren:()=>import('./services/services.module').then(res=>res.ServicesModule)},
+  {path:'manageproduct',component:ManageproductsComponent}
 ];
 
 @NgModule({
