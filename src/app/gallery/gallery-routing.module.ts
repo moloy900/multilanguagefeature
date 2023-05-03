a1139777b897f4e6da4d53aa6116ca06630db176
bleads2 <bleads2@gmail.com>
2023-05-02 22:49:53 -0700

    gallery routing configure

---
+++ src/app/gallery/gallery-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TVComponent } from './tv/tv.component';
+import { LaptopComponent } from './laptop/laptop.component';
+import { MobileComponent } from './mobile/mobile.component';
+import { WashingMachineComponent } from './washing-machine/washing-machine.component';
+
+const routes: Routes = [
+  {path:'tv',component:TVComponent},
+  {path:'laptop',component:LaptopComponent},
+  {path:'mobile',component:MobileComponent},
+  {path:'washingmachine',component:WashingMachineComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class GalleryRoutingModule { }
