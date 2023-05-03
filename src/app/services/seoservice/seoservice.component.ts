9ad74ed63aa2a2b9030a83d401a97bd652f87a3d
bleads2 <bleads2@gmail.com>
2023-05-02 23:37:39 -0700

    services feature module create and router configure

---
+++ src/app/services/seoservice/seoservice.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seoservice',
+  templateUrl: './seoservice.component.html',
+  styleUrls: ['./seoservice.component.css']
+})
+export class SeoserviceComponent {
+
+}
---
+++ src/app/services/webdesign/webdesign.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-webdesign',
+  templateUrl: './webdesign.component.html',
+  styleUrls: ['./webdesign.component.css']
+})
+export class WebdesignComponent {
+
+}
