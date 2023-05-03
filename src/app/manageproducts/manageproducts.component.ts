77d2ba356c4e5be2ecaad8f10ce15fda92640109
bleads2 <bleads2@gmail.com>
2023-05-02 23:44:06 -0700

    manageproduct component created

---
+++ src/app/manageproducts/manageproducts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manageproducts',
+  templateUrl: './manageproducts.component.html',
+  styleUrls: ['./manageproducts.component.css']
+})
+export class ManageproductsComponent {
+
+}
