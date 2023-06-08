8d9ce2b179d516e321c6267e7c06663fb7132401
Moloy <bleads2@gmail.com>
2023-06-08 01:08:00 -0700

    added fragment for route parameter

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-aboutus',
   templateUrl: './aboutus.component.html',
-  styleUrls: ['./aboutus.component.css']
+  styleUrls: ['./aboutus.component.css'],
 })
-export class AboutusComponent {
+export class AboutusComponent implements OnInit {
+  constructor(private activateroute: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    this.activateroute.fragment.subscribe((res) => {
+      console.log(res);
+      this.JumpTo(res);
+    });
+  }
+
+  JumpTo(section: any) {
+    setTimeout(() => {
+      document.getElementById(section)?.scrollIntoView({ behavior: 'smooth' });
+      // console.log("hello click"+section)
+    }, 1000);
+  }
 }
